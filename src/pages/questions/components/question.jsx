7fe2aace40e7cd2e138c77a67f8b2c33172af539
irwanadinata <irwanadinata9@gmail.com>
2024-06-24 18:26:58 +0700

    chore: add number question

--- src/pages/questions/components/question.jsx
+++ src/pages/questions/components/question.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Question = ({ question, selectAndSubmitAnswer }) => (
   <div className="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-lg">
-    <h2 className="text-2xl font-bold mb-4">{question.title}</h2>
+    <h2 className="text-2xl font-bold mb-4">{question.id-3}.{question.title}</h2>
     <div className="grid grid-cols-1 gap-4">
       <button
         className="px-4 py-2 bg-[#293066] text-white rounded-lg hover:bg-slate-600 transition"
